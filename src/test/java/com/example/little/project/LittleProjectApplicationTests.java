c88acd78104aff8d112ab9bb827f355cde72a7ea
edavud <enisdavud@gmail.com>
2022-12-05 18:13:33 -0500

    Initial commit

---
+++ src/test/java/com/example/little/project/LittleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.little.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
